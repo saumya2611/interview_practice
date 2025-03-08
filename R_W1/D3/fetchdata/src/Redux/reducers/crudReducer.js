8750679af42abc59191582e58a30cc907c3c3769
saumya2611 <somu0438@gmail.com>
2025-03-08 22:55:26 +0530

    some changes

--- R_W1/D3/fetchdata/src/Redux/reducers/crudReducer.js
+++ R_W1/D3/fetchdata/src/Redux/reducers/crudReducer.js
@@ -1,92 +1,92 @@
-import {
-  DELETE_DATA,
-  EDIT_CRUD,
-  FETCH_DATA,
-  SEARCH_DATA,
-  SHORTING,
-} from "../action";
+// import {
+//   DELETE_DATA,
+//   EDIT_CRUD,
+//   FETCH_DATA,
+//   SEARCH_DATA,
+//   SHORTING,
+// } from "../action";
 
-const initalState = {
-  fetchData: [],
-  filterData: [],
-  searchedValue: "",
-  order: "",
-};
+// const initalState = {
+//   fetchData: [],
+//   filterData: [],
+//   searchedValue: "",
+//   order: "",
+// };
 
-export const crudReducers = (state = initalState, action) => {
-  const { type, payload, id } = action;
-  //   console.log("crudReducers   id----------> ", id);
+// export const crudReducers = (state = initalState, action) => {
+//   const { type, payload, id } = action;
+//   //   console.log("crudReducers   id----------> ", id);
 
-  console.log("crudReducers   payload----------> ", payload);
+//   console.log("crudReducers   payload----------> ", payload);
 
-  switch (type) {
-    case FETCH_DATA:
-      return {
-        ...state,
-        fetchData: payload,
-        filterData: payload,
-      };
+//   switch (type) {
+//     case FETCH_DATA:
+//       return {
+//         ...state,
+//         fetchData: payload,
+//         filterData: payload,
+//       };
 
-    case EDIT_CRUD:
-      const updateData = state.filterData.map((item) => {
-        // console.log("item----------->", item);
+//     case EDIT_CRUD:
+//       const updateData = state.filterData.map((item) => {
+//         // console.log("item----------->", item);
 
-        if (item.id === id) {
-          return {
-            ...item,
-            title: payload,
-          };
-        }
-        return item;
-      });
-      console.log("updateData------------>", updateData);
-      return {
-        ...state,
-        fetchData: updateData,
-        filterData: updateData,
-      };
+//         if (item.id === id) {
+//           return {
+//             ...item,
+//             title: payload,
+//           };
+//         }
+//         return item;
+//       });
+//       console.log("updateData------------>", updateData);
+//       return {
+//         ...state,
+//         fetchData: updateData,
+//         filterData: updateData,
+//       };
 
-    case DELETE_DATA:
-      const delItem = state.filterData.filter((item) => {
-        return item.id != payload;
-      });
+//     case DELETE_DATA:
+//       const delItem = state.filterData.filter((item) => {
+//         return item.id != payload;
+//       });
 
-      console.log("delItem---------->", delItem);
-      return {
-        ...state,
-        filterData: delItem,
-      };
+//       console.log("delItem---------->", delItem);
+//       return {
+//         ...state,
+//         filterData: delItem,
+//       };
 
-    case SEARCH_DATA:
-      const newSearchValue = state.filterData.filter((item) => {
-        return item.title.toLowerCase().includes(payload.toLowerCase());
-      });
-      console.log("newSearchValue--------->", newSearchValue);
+//     case SEARCH_DATA:
+//       const newSearchValue = state.filterData.filter((item) => {
+//         return item.title.toLowerCase().includes(payload.toLowerCase());
+//       });
+//       console.log("newSearchValue--------->", newSearchValue);
 
-      return {
-        ...state,
-        filterData: newSearchValue,
-        searchedValue: payload,
-      };
+//       return {
+//         ...state,
+//         filterData: newSearchValue,
+//         searchedValue: payload,
+//       };
 
-    case SHORTING:
-      const shortdata = [...state.filterData].sort((a, b) => {
-        if (a[payload] === "number") {
-          if (state.order === "asc") {
-            return a[payload] - b[payload];
-          } else {
-            return b[payload] - a[payload];
-          }
-        }
-      });
+//     case SHORTING:
+//       const shortdata = [...state.filterData].sort((a, b) => {
+//         if (a[payload] === "number") {
+//           if (state.order === "asc") {
+//             return a[payload] - b[payload];
+//           } else {
+//             return b[payload] - a[payload];
+//           }
+//         }
+//       });
 
-      return {
-        ...state,
-        order: action.order === "asc" ? "dsc" : "asc",
-        filterData: shortdata,
-      };
+//       return {
+//         ...state,
+//         order: action.order === "asc" ? "dsc" : "asc",
+//         filterData: shortdata,
+//       };
 
-    default:
-      return state;
-  }
-};
+//     default:
+//       return state;
+//   }
+// };
