35f436802445baa2f0abf55eba59408e3209bcd3
saumya2611 <somu0438@gmail.com>
2025-03-09 23:32:49 +0530

    some changes

--- R_W1/D4/todo/src/Redux/Reducers/TodoReducers.jsx
+++ R_W1/D4/todo/src/Redux/Reducers/TodoReducers.jsx
@@ -1,85 +1,85 @@
-import { CREATE_TODO, DELETE_TODO, EDIT_TODO, RESTORE_TODO } from "../action";
+// import { CREATE_TODO, DELETE_TODO, EDIT_TODO, RESTORE_TODO } from "../action";
 
-const initialValue = {
-  todoArr: [],
-  resArr: [],
-  idCnt: 1,
-};
+// const initialValue = {
+//   todoArr: [],
+//   resArr: [],
+//   idCnt: 1,
+// };
 
-export const todoReducer = (state = initialValue, action) => {
-  const { type, payload, newValue } = action;
-  // console.log("newValue-------->", newValue);
+// export const todoReducer = (state = initialValue, action) => {
+//   const { type, payload, newValue } = action;
+//   // console.log("newValue-------->", newValue);
 
-  switch (type) {
-    case CREATE_TODO:
-      const todoObj = {
-        value: payload,
-        id: state.idCnt,
-      };
-      // console.log("todoObj-------->", todoObj);
+//   switch (type) {
+//     case CREATE_TODO:
+//       const todoObj = {
+//         value: payload,
+//         id: state.idCnt,
+//       };
+//       // console.log("todoObj-------->", todoObj);
 
-      return {
-        ...state,
-        todoArr: [...state.todoArr, todoObj],
-        idCnt: state.idCnt + 1,
-      };
+//       return {
+//         ...state,
+//         todoArr: [...state.todoArr, todoObj],
+//         idCnt: state.idCnt + 1,
+//       };
 
-    case EDIT_TODO:
-      const updatedValue = state.todoArr.map((item) => {
-        if (item.id === payload.id) {
-          return {
-            value: newValue,
-            id: payload.id,
-          };
-        }
+//     case EDIT_TODO:
+//       const updatedValue = state.todoArr.map((item) => {
+//         if (item.id === payload.id) {
+//           return {
+//             value: newValue,
+//             id: payload.id,
+//           };
+//         }
 
-        return item;
-      });
-      // console.log("updatedValue----------->", updatedValue);
+//         return item;
+//       });
+//       // console.log("updatedValue----------->", updatedValue);
 
-      return {
-        ...state,
-        todoArr: updatedValue,
-      };
+//       return {
+//         ...state,
+//         todoArr: updatedValue,
+//       };
 
-    case DELETE_TODO:
-      const deleteItem = state.todoArr.filter((item) => {
-        return item.id != payload.id;
-      });
+//     case DELETE_TODO:
+//       const deleteItem = state.todoArr.filter((item) => {
+//         return item.id != payload.id;
+//       });
 
-      const restoreItem = state.todoArr.find((item) => {
-        return item.id === payload.id;
-      });
+//       const restoreItem = state.todoArr.find((item) => {
+//         return item.id === payload.id;
+//       });
 
-      console.log("restoreItem---------->", restoreItem);
+//       console.log("restoreItem---------->", restoreItem);
 
-      console.log("deleteItem==========>", deleteItem);
+//       console.log("deleteItem==========>", deleteItem);
 
-      return {
-        ...state,
-        todoArr: deleteItem,
-        resArr: [...state.resArr, restoreItem],
-      };
+//       return {
+//         ...state,
+//         todoArr: deleteItem,
+//         resArr: [...state.resArr, restoreItem],
+//       };
 
-    case RESTORE_TODO:
-      const restoreData = state.resArr.filter((item) => {
-        return item.id != payload.id;
-      });
+//     case RESTORE_TODO:
+//       const restoreData = state.resArr.filter((item) => {
+//         return item.id != payload.id;
+//       });
 
-      const restoreList = state.resArr.find((item) => {
-        return item.id === payload.id;
-      });
+//       const restoreList = state.resArr.find((item) => {
+//         return item.id === payload.id;
+//       });
 
-      console.log("restoreList==========>", restoreList);
-      console.log("restoreData==========>", restoreData);
+//       console.log("restoreList==========>", restoreList);
+//       console.log("restoreData==========>", restoreData);
 
-      return {
-        ...state,
-        resArr: restoreData,
-        todoArr: [...state.todoArr, restoreList],
-      };
+//       return {
+//         ...state,
+//         resArr: restoreData,
+//         todoArr: [...state.todoArr, restoreList],
+//       };
 
-    default:
-      return state;
-  }
-};
+//     default:
+//       return state;
+//   }
+// };
